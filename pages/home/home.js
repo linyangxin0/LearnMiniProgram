57f2a3f8814654b8d26d03574887667234e7bd91
linyangxin <1227143051@qq.com>
2020-03-27 23:23:17 +0800

    image标签的使用

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    imageURL:''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  handleChooseAlbum(){
+    // 系统api让系统在相册中选择图片或拍照
+    wx.chooseImage({
+      complete: (res) => {
+        const path=res.tempFilePaths[0]
+        this.setData({
+          imageURL:path
+        })
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handImageLoad(){
+    console.log("图片加载完成")
   }
 })
