62feff7ecdfb221e5927ff8a695a377dada26440
linyangxin <1227143051@qq.com>
2020-03-30 22:30:16 +0800

    详情页基本完成

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,18 +1,77 @@
 // pages/detail/detail.js
+import {getDetail,getRecommends,GoodsBaseInfo,ShopInfo,ParamInfo} from '../../service/detail.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    iid:'1m7rp9w',
+    topImages: [],
+    baseInfo: {},
+    shopInfo: {},
+    detailInfo: {},
+    paramInfo: {},
+    commentInfo: {},
+    recommends: {},
+    isBackTopShow:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // this.setData({
+    //   iid:options.iid
+    // })
 
+    this._getDetail();
+    
+    this._getRecommends();
+  },
+
+  _getDetail(){
+    getDetail(this.data.iid).then(res=>{
+      const data = res.data.result;
+
+      // 1.取出顶部的图片
+      const topImages = data.itemInfo.topImages;
+
+      // 2.创建BaseInfo对象
+      const baseInfo = new GoodsBaseInfo(data.itemInfo, data.columns, data.shopInfo.services)
+
+      // 3.创建ShopInfo对象
+      const shopInfo = new ShopInfo(data.shopInfo);
+
+      // 4.获取detailInfo信息
+      const detailInfo = data.detailInfo;
+
+      // 5.创建ParamInfo对象
+      const paramInfo = new ParamInfo(data.itemParams.info, data.itemParams.rule)
+
+      // 6.获取评论信息
+      let commentInfo = {}
+      if (data.rate && data.rate.cRate > 0) {
+        commentInfo = data.rate.list[0]
+      }
+
+      this.setData({
+        topImages: topImages,
+        baseInfo: baseInfo,
+        shopInfo: shopInfo,
+        detailInfo: detailInfo,
+        paramInfo: paramInfo,
+        commentInfo: commentInfo
+      })
+    })
+  },
+  _getRecommends() {
+    getRecommends().then(res => {
+      this.setData({
+        recommends: res.data.data.list
+      })
+    })
   },
 
   /**
@@ -62,5 +121,15 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onPageScroll(options){
+    const scrollTop=options.scrollTop
+    const flag1 = scrollTop>1000
+    if(flag1!=this.data.isBackTopShow){
+      this.setData({
+        isBackTopShow:scrollTop>1000
+      })
+    }
   }
 })
